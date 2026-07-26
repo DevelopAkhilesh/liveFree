2cbe34016ac278b4b3dad18d45310a2e0639d5b0
akhilesh guleria <developakhilesh01@gmail.com>
2026-07-26 12:53:06 +0530

    added the backtotop component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import ScrollToTop from './components/ScrollToTop'
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton'
+import BackToTop from './components/BackToTop/BackToTop'
 
 export default function Layout() {
   return (
@@ -12,6 +13,7 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
+      <BackToTop />
       <WhatsAppButton />
       <Footer />
     </>
