f3b486bf78ec9495c35fed09d55f0b431824e92a
akhilesh guleria <developakhilesh01@gmail.com>
2026-08-16 14:29:14 +0530

    update in the config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/liveFree/',   // was '/' — must match repo name exactly, capital F included
+  base: '/',   // was '/' — must match repo name exactly, capital F included
   plugins: [react()],
 })
